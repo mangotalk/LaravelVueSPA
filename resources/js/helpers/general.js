5698fb0c9b925112a535d9a1c17ca72c921c92bb
Pascal Hesselink <mail@pascalhesselink.nl>
2019-03-19 09:14:26 +0100

    Removed register route access when already logged in

--- resources/js/helpers/general.js
+++ resources/js/helpers/general.js
@@ -2,16 +2,18 @@ export function initialize(store, router) {
     router.beforeEach((to, from, next) => {
         const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
         const currentUser = store.state.currentUser;
-    
+
         if(requiresAuth && !currentUser) {
             next('/login');
         } else if(to.path == '/login' && currentUser) {
+            next('/');
+        } else if(to.path == '/register' && currentUser) {
             next('/');
         } else {
             next();
         }
     });
-    
+
     if (store.getters.currentUser) {
         setAuthorization(store.getters.currentUser.token);
     }
